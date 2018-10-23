4486c9e9b3bcb84ac6d72b42d41217982d9da6ae
WolfGirlM <18829491124@163.com>
2018-10-23 16:05:22 +0800

    :zap:读入链表

--- PTA/mergeLink.c
+++ PTA/mergeLink.c
@@ -9,7 +9,7 @@ struct Node {
 };
 typedef PtrToNode List;
 
-List Read(List C,int a[],int n); /* 读入有序链表 */
+List Read(List C); /* 读入有序链表 */
 void Print( List L ); /* 输出链表；空链表将输出NULL */
 
 // List Merge( List L1, List L2 );
@@ -19,33 +19,33 @@ int main()
     List L1, L2, L,C;
     int n;
     int a[200];
-    L1 = Read(C,a,n);
-    L2 = Read(C,a,n);
-    L = Merge(L1, L2);
-    Print(L);
+    L1 = Read(C);
+    //L2 = Read(C);
+    //L = Merge(L1, L2);
+    //Print(L);
     Print(L1);
-    Print(L2);
+    //Print(L2);
     return 0;
 }
 
 /* 你的代码将被嵌在这里 */
-List Read(List C,int a[],int n)
+List Read(List C)
 {
-    List s,r;
-    int x;
-    C = (List*)malloc(sizeof(List));//给链表头结点分配空间
+    struct Node *s;
+    int x,n;
+    C = (struct Node*)malloc(sizeof(struct Node));//给链表头结点分配空间
     C->Next = NULL;
-    r = C;
-    for(int i=0; i<n; i++)
+    scanf("%d",&n);
+    scanf("%d",&x);
+    for(int i=1; i<n; i++)
     {
-        scanf("%d",&x);
-        a[i] = x;
-        s = (List *)malloc(sizeof(List));
-        s->Data = a[i];
-        r->Next = s;
-        r = r->Next;
+        s = (struct Node*)malloc(sizeof(struct Node));
+        s->Data = x;
+        s->Next = C->Next;
+        C->Next = s;
+        scanf("%d", &x);
     }
-    r->Next = NULL;
+    return C;
 }
 void Print( List L )
 {
@@ -54,6 +54,10 @@ void Print( List L )
     {
         printf("%d\n",t->Data);
         t = t->Next;
+    }
+    else
+    {
+        printf("NULL\n");
     }
 
 }
